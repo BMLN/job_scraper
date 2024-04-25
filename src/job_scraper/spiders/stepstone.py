c52eb630ca541aa58cba53ceeed507247e11d1f5
BMLN <lorandbanki@gmail.com>
2024-04-25 17:05:52 +0200

    stepstone running from cmp-ids

--- src/job_scraper/spiders/stepstone.py
+++ src/job_scraper/spiders/stepstone.py
@@ -7,6 +7,51 @@ import logging
 
 
 
+mappings_comp = {
+    "Adidas": "adidas-AG-41866",
+    "Airbus": "Airbus-Group-500",
+    "Allianz": "allianz-versicherungs-ag-30958",
+    "BASF": "BASF-SE-7616",
+    "Bayer": "bayer-318068",
+    "Beiersdorf": "Beiersdorf-AH-8453",
+    "BMW": "BMW-Group-27361",
+    "Brenntag": "Brenntag-GmbH-24117",
+    "Commerzbank": "Commerzbank-AG-9509",  
+    "Continental": "Continental-AG-3975",
+    "Covestro": None, #none
+    "Daimler Truck": "Daimler-Truck-AG-283584",
+    "Deutsche Bank": "deutsche-bank-33645",
+    "Deutsche Börse": "deutsche-börse-group-167150",
+    "Deutsche Post": "deutsche-post-ag-136538",
+    "Deutsche Telekom": None, #none
+    "E.ON": "eon-2346",
+    "Fresenius": "fresenius-se-&-co-kgaa-9881",
+    "Hannover Rück": "hannover-rück-se-4647",
+    "Heidelberg Materials": "heidelberg-materials-ag-25005",
+    "Henkel": "henkel-ag-&-co-kgaa-28132",
+    "Infineon": None, #none
+    "Mercedes-Benz Group": "mercedes-benz-group-ag-25115",
+    "Merck": "merck-kgaa-darmstadt_germany-5838",
+    "MTU Aero Engines" : "MTU-Aero-Engines-AG-33244",
+    "Münchner Rück" : None, #no matches,
+    "Porsche AG" : "porsche-ag-4166",
+    "Porsche SE" : "porsche-automobil-holding-se-130455",
+    "Qiagen": "qiagen-gmbh-1827",
+    "Rheinmetall": "rheinmetall-1262",
+    "RWE": "rwe-generation-se-148648",
+    "SAP": "sap-se-215050",
+    "Sartorius": "sartorius-32291",
+    "Siemens": "siemens-ag-4831",
+    "Siemens Energy" : "siemens-gamesa-renewable-energy-220386",
+    "Siemens Healthineers" : "siemens-healthineers-ag-358280",
+    "Symrise": "symrise-ag-112974",
+    "Volkswagen" : "volkswagen-infotainment-gmbh-148893",
+    "Vonovia": "vonovia-76597",
+    "Zalando": "zalando-se-71523"
+}
+
+
+#TODO: none cmps arent filtered
 class Stepstone_JobSearch_Scraper(templ.JobSearch_Scraper):
     name = "stepstone_jobsearch_spider"
     allowed_domains = ["stepstone.de"]
@@ -70,9 +115,11 @@ class Stepstone_JobSearch_Scraper(templ.JobSearch_Scraper):
     def searchurl_for(self, company, is_company=False):
         #return self.__search_url + str(company)
         base = self.__general_search_url if is_company is False else self.__company_search_url
+        base = self.__company_search_url
+
         #return "file:///Users/lorandbanki/Desktop/Arbeit/job_scraper/xpathstuff2.html"
         #return "file:///Users/lorandbanki/Desktop/Arbeit/job_scraper/jobsite.html"
-        url = templ.Url(base.format(company = company))\
+        url = templ.Url(base.format(company = mappings_comp[company]))\
             .param("fu", 1000000)\
             .param("ct", 229)
 
