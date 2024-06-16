365cd6473c1394a0eac371b3a746d3f89c5c67da
BMLN <lorandbanki@gmail.com>
2024-06-16 22:46:07 +0200

    monster.de spider

---
+++ src/job_scraper/spiders/monster.py
@@ -0,0 +1,95 @@
+from job_scraper.core import new_templ
+from typing import override
+
+from scrapy.linkextractors import LinkExtractor
+from urllib.parse import unquote
+
+
+
+#TODO: javascript
+class Monster_JobScraper(new_templ.JobSearchScraper):
+    
+    name = "monster_jobsearch_spider"
+    allowed_domains = ["monster.de"]
+
+    __extractor = LinkExtractor(
+        restrict_xpaths = "//div[@id='JobCardGrid']//a[@data-testid='jobTitle']",
+    )
+
+
+    
+
+    #interface requirements
+
+    @classmethod
+    @override
+    def url_extractor(cls, response) -> [dict]:
+        return [ {"url_text": url.text, "url" : url.url} for url in cls.__extractor.extract_links(response) ]
+
+
+    #TODO:javascript
+    @classmethod
+    @override
+    def nextractor(cls, response) -> str:
+        pass
+
+
+
+
+
+
+
+
+class Monster_InfoScraper(new_templ.JobInfoScraper):
+
+    name = "monster_jobinfo_spider"
+    allowed_domains = ["monster.de"]
+    
+
+
+
+    #interface requirements
+    
+    @override
+    def extract_jobtitle(cls, selector) -> str:
+        return selector.xpath("//div[@id='job-view-header']//h2[@data-testid='jobTitle']//text()").get()
+        
+    @override
+    def extract_content(cls, selector) -> str:
+        return selector.xpath("//div[contains(@class, 'DescriptionContainer')]//div//div").get()
+        
+    @override
+    def extract_company(cls, selector) -> str:
+        output = selector.xpath("//div[@id='job-view-header']//li[@data-testid='company']//text()").get()
+
+        return output
+
+    @override
+    def extract_field(cls, selector) -> str:
+        return None
+
+    @override
+    def extract_industry(cls, selector) -> str:
+        output = selector.xpath("//div[contains(@class, 'numbers-and-facts')]//table//tr")
+        
+        if len(output) > 0:
+            output = output[2].xpath(".//td//text()")
+            if output:
+                return output[1]
+
+    @override
+    def extract_employment(cls, selector) -> str:
+        output = selector.xpath("//div[contains(@class, 'numbers-and-facts')]//table//tr")
+        
+        if len(output) > 0:
+            output = output[1].xpath(".//td//text()")
+            if output:
+                return output[1]
+
+    @override
+    def extract_location(cls, selector) -> str:
+        return selector.xpath("//div[@id='job-view-header']//li[@data-testid='jobDetailLocation']//text()")
+
+    @override
+    def extract_posting(cls, selector) -> str:
+        return selector.xpath("//div[@id='job-view-header']//li[@data-testid='jobDetailDataRecency']//text()")
