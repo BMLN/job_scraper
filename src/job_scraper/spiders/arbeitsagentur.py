057d3427c4846bbbbc284cfed53f8462fff339ff
BMLN <lorandbanki@gmail.com>
2024-06-17 15:09:28 +0200

    arbeitsagentur.de spider

--- src/job_scraper/spiders/arbeitsagentur.py
+++ src/job_scraper/spiders/arbeitsagentur.py
@@ -1,4 +1,4 @@
-from job_scraper.spiders import templ
+from job_scraper.core import new_templ
 import scrapy 
 
 from typing import override, Iterable
@@ -13,7 +13,7 @@ class Arbeitsagentur_JobSearch_Scraper(templ.JobSearch_Scraper):
     __general_search_url = "https://www.arbeitsagentur.de/jobsuche/suche"
     __extractor = scrapy.linkextractors.LinkExtractor(
         restrict_xpaths = "//div[@id = 'ergebnisliste']",
-        restrict_text = "student"
+        #restrict_text = "student"
     )
 
 
@@ -21,31 +21,9 @@ class Arbeitsagentur_JobSearch_Scraper(templ.JobSearch_Scraper):
     # interface requirements
 
     @override
-    def searchurl_for(self, company):
-        #str(company) +  + " Werkstudent")
-        url = templ.Url(self.__general_search_url.format())\
-            .param("was", str(company))\
-            .param("angebotsart", 34)\
-            .param("berufsfeld", "Informatik")
+    def url_extractor(self, selector) -> list[dict]:
+        return [ {"url_text": url.text, "url" : url.url} for url in cls.__extractor.extract_links(response) ]
 
-        return str(url)
-
-
-    @override
-    def extract_source(self, response):
-        ret = re.search("was=(.*?)&", response.request.url)
-        if ret != None:
-            ret = ret.group()[4:-1]
-
-        return ret
-
-
-    @override
-    def extract_joburls(self, selector):
-
-        links = self.__extractor.extract_links(selector)
-
-        return [ str(x.url) for x in links ]
 
 
     #TODO: javascript interaction
@@ -62,67 +40,41 @@ class Arbeitsagentur_JobInfo_Scraper(templ.JobInfo_Scraper):
     name = "arbeitsagentur_jobinfo_spider"
     allowed_domains = ["arbeitsagentur.de"]
 
+
+
+
     
     # Interface Implementations
 
     @override
     def extract_jobtitle(self, selector):
+        return selector.xpath("//div[@id='detail-kopfbereich-titel']//text()").get()
+                 
+    @override
+    def extract_content(cls, selector) -> str:
+        return selector.xpath("//div[@class='ba-layout-tile']//p").get()
         
-        output = selector.xpath("//div[@id='detail-kopfbereich-titel']//text()").get()
-        
-        return output
-
+    @override
+    def extract_company(cls, selector) -> str:
+        return selector.xpath("//span[@id='detail-kopfbereich-firma']//text()").get()
 
     @override
-    def extract_company(self, selector):
-        
-        output = selector.xpath("//span[@id='detail-kopfbereich-firma']//text()").get()
-        
-        return output
-
-
-    @override
-    def extract_location(self, selector):
-        
-        output = selector.xpath("//span[@id='detail-kopfbereich-arbeitsort']//text()").get()
-        
-        return output
-
-
-    @override
-    def extract_employment(self, selector):
-        
-        output = selector.xpath("//span[@id='detail-kopfbereich-angebotsart']//text()").get()
-        
-        return output
-
-
-    @override
-    def extract_tasks(self, selector):
-
+    def extract_field(cls, selector) -> str:
         return None
 
-
     @override
-    def extract_qualifications(self, selector):
-        
+    def extract_industry(cls, selector) -> str:
         return None
 
+    @override
+    def extract_employment(cls, selector) -> str:
+        return selector.xpath("//span[@id='detail-kopfbereich-angebotsart']//text()").get()
 
     @override
-    def extract_etc(self, selector):
-                
-        #return "-"
-        #output = selector.xpath("//div[@class='ba-layout-tile']//child:**[2]")
-        #print(output)
-        #print(output.getall())
-        return None
+    def extract_location(cls, selector) -> str:
+        return selector.xpath("//span[@id='detail-kopfbereich-arbeitsort']//text()").get()
 
-    
     @override
-    def extract_rawtext(self, selector):
-        #output = selector.xpath("//p[@id='detail-beschreibung-beschreibung']//child::*").getall()
-        output = selector.xpath("//div[@class='ba-layout-tile']//text()").getall()
-        
+    def extract_posting(cls, selector) -> str:
+        return selector.xpath("//span[@id='detail-kopfbereich-veroeffentlichungsdatum']//text()").get()
 
-        return output
