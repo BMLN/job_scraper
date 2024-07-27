77151d65d011bdab7b2797216e05586b986f2b61
BMLN <lorandbanki@gmail.com>
2024-07-27 13:12:03 +0200

    WIP cf: does the jobs, but not using the way intended

--- src/job_scraper/middlewares.py
+++ src/job_scraper/middlewares.py
@@ -4,7 +4,7 @@
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
-from scrapy.http import Response, HtmlResponse, TextResponse
+from scrapy.http import Response, HtmlResponse, TextResponse, Request
 from scrapy.spidermiddlewares.httperror import HttpErrorMiddleware
 
 # useful for handling different item types with a single interface
@@ -12,8 +12,10 @@ from itemadapter import is_item, ItemAdapter
 
 
 from cloudscraper import create_scraper
-import queue
+
 import pandas as pd
+from urllib.parse import urlparse
+
 
 class JobScraperSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
@@ -147,7 +149,11 @@ def parse_inputs(input):
 #TODO: pass delay/etc from settings.py
 class CloudFlareMiddleware:
     
-    scraper = create_scraper()
+    browser = {
+        "mobile": False,
+        "desktop": True
+    }
+    scraper = create_scraper(debug=False)
 
     #def __init__(self, settings={}):
     #    self.scraper = create_scraper(debug=False)
@@ -163,35 +169,199 @@ class CloudFlareMiddleware:
 
     def process_request(self, request, spider):
         output = HtmlResponse(request.url)
+        output.status = 500
         url = request.url
         proxies = {} #cloudscraper provides proxylist
                 
         #print("requesting", request.url)
-        for x in range(100000000): #super cool very gud delay func
-                pass
+        for x in range(400000000): #super cool very gud delay func
+            pass
 
-        with self.scraper.get(url=url, proxies=proxies, timeout=60*2) as resp:
-            output = HtmlResponse(
-                resp.url, 
-                status=resp.status_code, 
-                headers=resp.headers, 
-                #body=resp.content, #problem?
-                body=str(resp.content),
-                encoding="utf-8",
-                request=request
-            )
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate"
+        } 
+        try:
+            with CloudFlareMiddleware.scraper.get(url=url, proxies=proxies, timeout=60*2) as resp:
+                output = HtmlResponse(
+                    resp.url, 
+                    status=resp.status_code, 
+                    headers=resp.headers, 
+                    #body=resp.content, #problem?
+                    body=str(resp.content),
+                    encoding="utf-8",
+                    request=request,
+                    #meta= {"dont_redirect": True}
+                )
 
-            if """<span id="challenge-error-text">Enable JavaScript and cookies to continue</span>""" in str(output.body):
-                print("didnt solve challenge")
-                output.status = 500
+                #TODO: add url check if still base
+                
+
+                if """<span id="challenge-error-text">Enable JavaScript and cookies to continue</span>""" in str(output.body):
+                    print("didnt solve challenge")
+                    output.status = 500
+            
+                if """<h1 data-translate="block_headline">Sorry, you have been blocked</h1>""" in str(output.body):
+                    print("blocked on used ip")
+                    output.status = 503
         
-            if """<h1 data-translate="block_headline">Sorry, you have been blocked</h1>""" in str(output.body):
-                print("blocked on used ip")
-                output.status = 503
-
+        except:
+            pass
+        
+        
         #print(resp.content)
         return output
 
 # class ErrorMiddleware(HttpErrorMiddleware):
     
 #     @override
+
+
+
+
+
+#TODO: read from settings and set defaults
+class CloudFlareMiddleware2:
+    
+    CFM_SESSION_INIT_THRESHOLD = 5
+    CFM_SESSION_EVAL_THRESHOLD = 0.8
+    
+    CFM_MULTI_THRESHOLD = 3
+    CFM_MULTI_MINSESSIONS = 3
+    CFM_MULTI_MAXSESSIONS = 10
+
+    CFM_MAX_CREATED_SESSIONS = 20
+
+
+    def __init__(self, settings={}):
+        self.__sessions = []
+        #TODO: settings init
+        #self.CFM_MULTI_THRESHOLD = 3
+        #self.CFM_MAX_CREATED_SESSIONS = 20
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings)
+
+    def new_session(self, *args, **kwargs):
+        self.__sessions.append((new_sess := self.CfSession(len(self.__sessions) + 1, *args, **kwargs)))
+        
+        return new_sess
+
+    #TODO: cleaner
+    def eval_session(self, session):
+        if session.fails + session.success < self.CFM_SESSION_INIT_THRESHOLD:
+            return True
+        elif session.success / (session.fails + session.success) < self.CFM_SESSION_EVAL_THRESHOLD:
+            return False
+        else:
+            return True 
+
+
+    def get_sessions(self, session_key=0, retry_times=0):
+        output = []
+        session_key = session_key if session_key else 0
+        retry_times = retry_times if retry_times else 0
+
+        if self.__sessions and self.eval_session(self.__sessions[session_key]) and retry_times < self.CFM_MULTI_THRESHOLD:
+            output.append(self.__sessions[session_key])
+        else:
+            evaled_sessions = [ session for session in self.__sessions if self.eval_session(session) ]
+            new_sessions = [ self.new_session() for x in range(self.CFM_MULTI_MINSESSIONS - len(evaled_sessions)) ] #TODO: CFM_MAX_CREATED_SESSIONS
+            
+            output += evaled_sessions[session_key:] + new_sessions + evaled_sessions[:session_key]
+                #sess = self.__sessions[ (session_key + x) % len(self.__sessions) ]
+            
+        print(f"{len(self.__sessions)} total sessions, {len([ session for session in self.__sessions if self.eval_session(session) ])} working")
+        #while len(output) < self.CFM_MULTI_MINSESSIONS and len(output) < self.CFM_MULTI_MAXSESSIONS and len(self.__sessions) < self.CFM_MAX_CREATED_SESSIONS:
+        #    output.append(self.new_sess())
+
+        return output
+
+
+    def process_request(self, request, spider):
+        
+        for session in self.get_sessions(request.meta.get("session_key"), request.meta.get("retry_times")):
+            response = session.process_request(request)
+                
+            if 200 <= response.status < 300:
+                break
+        
+        #print(response.body)
+        return response
+
+
+
+    class CfSession():
+        def __init__(self, session_key, *args, **kwargs):
+            self.session = create_scraper(*args, **kwargs)
+            self.session_key = session_key
+            self.success = 0
+            self.fails = 0
+
+        @classmethod
+        def to_req_params(cls, request):
+            PARAMS = ["method", "url", "params", "data", "json", "headers", "cookies", "files", "auth", "timeout", "allow_redirects", "proxies", "verify", "stream", "cert"]
+            PARAMS.remove("headers")
+            return { key : value for key, value in { key if key[0] != "_" else key[1:] : value for key, value in request.__dict__.items() }.items() if key in PARAMS }
+
+
+        def send(self, *args, **kwargs):
+            for x in range(400000000): #super cool very gud delay func
+                pass
+
+            try:
+                with self.session.request(*args, **kwargs) as resp:
+                    return resp
+                
+
+            except Exception as e:
+                print(e)
+                return None
+            
+
+        def create_scrapyresponse(self, request, response):
+            output = HtmlResponse(request.url, status=500, request=request, encoding="utf-8")
+
+            #TODO: add url check if still base
+            if not response:
+                return output
+                
+            elif urlparse(request.url).hostname != urlparse(response.url).hostname:
+                print("got redirected")
+                return output
+
+            elif """<span id="challenge-error-text">Enable JavaScript and cookies to continue</span>""" in str(response.content):
+                print("didnt solve challenge")
+                return output
+            
+            elif """<h1 data-translate="block_headline">Sorry, you have been blocked</h1>""" in str(response.content):
+                print("blocked on used ip")
+                return output.replace(status = 503)
+
+            else:
+                return  HtmlResponse(
+                        response.url, 
+                        status=response.status_code, 
+                        headers=response.headers, 
+                        body=response.content,
+                        encoding="utf-8",
+                        request=request, #set afterwards? #not needed?
+                        #meta=request.meta | {"session_key" : self.session_key}#{"dont_redirect": True}
+                    )
+
+
+        def process_request(self, request):
+            params = self.to_req_params(request)
+
+            response = self.send(**params)
+            response = self.create_scrapyresponse(request, response)
+
+            if 200 <= response.status < 300:
+                self.success += 1
+            else:
+                self.fails += 1
+
+            return response
