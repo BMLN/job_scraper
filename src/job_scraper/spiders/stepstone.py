232a1662792ab1875aff0c1fe20fcf2dc133fc13
BMLN <lorandbanki@gmail.com>
2024-08-05 10:54:05 +0200

    stepstone.de spider

--- src/job_scraper/spiders/stepstone.py
+++ src/job_scraper/spiders/stepstone.py
@@ -1,9 +1,10 @@
-from job_scraper.core import new_templ
+from job_scraper.core import new_templ, url
 from typing import override
 
 from scrapy.linkextractors import LinkExtractor
 from urllib.parse import unquote
 
+import json
 
 
 class Stepstone_JobScraper(new_templ.JobSearchScraper):
@@ -11,12 +12,41 @@ class Stepstone_JobScraper(new_templ.JobSearchScraper):
     name = "stepstone_jobsearch_spider"
     allowed_domains = ["stepstone.de"]
 
-    __extractor = LinkExtractor(
-        #allow = "/www\.stepstone\.de\/stellenangebote--.*/",
-        allow = "/stellenangebote--", #some problems with regex, rly correct?
-        #restrict_xpaths = "//div[contains(@class, 'results')]"
-        restrict_xpaths = "(//div[@data-genesis-element = 'CARD_GROUP_CONTAINER'])[1]"
-    )
+    API_PAGESIZE = 0
+
+    API_BASE = 'https://www.stepstone.de/public-api/resultlist/unifiedResultlist'
+    API_HEADERS = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Origin': 'https://www.stepstone.de',
+        'Accept-Language': 'de-DE,de;q=0.9',
+        'Host': 'www.stepstone.de',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Safari/605.1.15',
+        'Referer': 'https://www.stepstone.de/jobs/werkstudent-in-it?q=Werkstudent%2fin+IT',
+        'Connection': 'keep-alive',
+    }
+    API_JSON = {
+        'url': None,
+        'lang': 'de',
+        'siteId': 250,
+        'userData': {
+            'isUserLoggedIn': False,
+            'candidateId': '',
+            'userHashId': "e5bcd192-0d0f-42dd-ae38-92baf934ddd6", #use a random uuid
+        },
+        'isNonEUUser': False,
+        'isBotCrawler': False,
+        'uiLanguage': 'de',
+        'fields': [
+            'items',
+            'pagination',
+            'unifiedPagination',
+        ],
+    }
+
+
+
+
 
 
     #interface requirements
@@ -24,20 +54,47 @@ class Stepstone_JobScraper(new_templ.JobSearchScraper):
     @classmethod
     @override
     def url_extractor(cls, response) -> [dict]:
-        return [ {"url_text": url.text, "url" : url.url} for url in cls.__extractor.extract_links(response) ]
+        return [ {"url_text": job.get("title"), "url" : f"{"https://https://www.stepstone.de"}{job.get("url")}"} for job in (response.json().get("items") or []) ]
 
 
-    #TODO: add filtercheck
     @classmethod
     @override
     def nextractor(cls, response) -> str:
-        nxt = selector.xpath("//nav[contains(@aria-label, 'pagination')]//a[@href]/@href").extract()
+        next = response.json().get("unifiedPagination", {}).get("links", {}).get("next")
+
+        if next:
+            return json.dumps(cls.API_JSON | { "url": next })
+
+
+    @override
+    def parse(cls, response):
+        api_call = url.Url(
+            base=cls.API_BASE,
+        )
         
-        return None #TODO: for testing
-        if len(nxt) > 1:
-            return nxt[-1]
-        else:
-            return None
+        yield response.follow(
+            str(api_call), 
+            method="POST",
+            headers=cls.API_HEADERS,
+            body= json.dumps(cls.API_JSON | { "url": response.url }),
+            callback=cls.parse_api
+        )
+        
+
+    @classmethod
+    def parse_api(cls, response):
+        for x in cls.url_extractor(response):
+            yield x
+                
+        if (next := cls.nextractor(response)): 
+            yield response.follow(
+                cls.API_BASE, 
+                method="POST",
+                headers=cls.API_HEADERS,
+                body=next,
+                callback=cls.parse_api
+            )
+
 
 
 
@@ -62,7 +119,6 @@ class Stepstone_InfoScraper(new_templ.JobInfoScraper):
     @classmethod
     @override
     def extract_content(cls, selector) -> str:
-        return None
         return selector.xpath("//div[@data-atx-component='JobAdContent']").get()
 
     @classmethod       
